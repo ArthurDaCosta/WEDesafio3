2f8402743da7e789907c2dee16895401c38c90fe
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-03-13 09:55:02 -0300

    carrinho

---
+++ ecommerce/public/js/carrinho.js
@@ -0,0 +1,16 @@
+/*-----------------------------------------------------------------*/
+const primeira = document.getElementById('primeira');
+primeira.addEventListener("mousemove", (e) =>{
+    console.log(e);
+    primeira.style.backgroundPositionX = (-e.screenX)/2 + "px";
+    primeira.style.backgroundPositionY = (-e.screenY)/2 + "px";
+});
+/*--------------------------------------------------------------------------------botão menu------------------*/
+document.getElementById("botao-menu").addEventListener("click", function() {
+var menu = document.getElementById("menu-retratil");
+if (menu.style.display === "" || menu.style.display === "none") {
+menu.style.display = "block";
+} else {
+menu.style.display = "none";
+}
+});
